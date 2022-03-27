f978b2c8a0184932f217bb518f996853fd4c9447
chmoralesm <102389633+chmoralesm@users.noreply.github.com>
2022-03-27 03:25:19 -0500

    Update preguntas.py

--- preguntas.py
+++ preguntas.py
@@ -11,6 +11,9 @@ Utilice el archivo `data.csv` para resolver las preguntas.
 
 
 """
+
+
+
 lista = []
 with open("data.csv", "r") as file:
     data_lab = file.readlines()
@@ -196,7 +199,93 @@ def pregunta_05():
     ]
 
     """
-    return
+    min_a = 0
+    min_b = 0
+    min_c = 0
+    min_d = 0
+    min_e = 0
+
+    max_a = 0
+    max_b = 0
+    max_c = 0
+    max_d = 0
+    max_e = 0
+
+    lista = []
+    with open("data.csv", "r") as file:
+        data_lab1 = file.readlines()
+        lista = list(data_lab1)
+
+    for linea in lista:
+        if linea[0] == "A":
+            if min_a != 0:
+                if min_a > linea[2]:
+                    min_a = linea[2]
+            else:
+                min_a = linea[2]
+        elif linea[0] == "B":
+            if min_b != 0:
+                if min_b > linea[2]:
+                    min_b = linea[2]
+            else:
+                min_b = linea[2]
+        elif linea[0] == "C":
+            if min_c != 0:
+                if min_c > linea[2]:
+                    min_c = linea[2]
+            else:
+                min_c = linea[2]
+        elif linea[0] == "D":
+            if min_d != 0:
+                if min_d > linea[2]:
+                    min_d = linea[2]
+            else:
+                min_d = linea[2]
+        elif linea[0] == "E":
+            if min_e != 0:
+                if min_e > linea[2]:
+                    min_e = linea[2]
+            else:
+                min_e = linea[2]
+
+    for linea in lista:
+        if linea[0] == "A":
+            if max_a != 0:
+                if max_a < linea[2]:
+                    max_a = linea[2]
+            else:
+                max_a = linea[2]
+        elif linea[0] == "B":
+            if max_b != 0:
+                if max_b < linea[2]:
+                    max_b = linea[2]
+            else:
+                max_b = linea[2]
+        elif linea[0] == "C":
+            if max_c != 0:
+                if max_c < linea[2]:
+                    max_c = linea[2]
+            else:
+                max_c = linea[2]
+        elif linea[0] == "D":
+            if max_d != 0:
+                if max_d < linea[2]:
+                    max_d = linea[2]
+            else:
+                max_d = linea[2]
+        elif linea[0] == "E":
+            if max_e != 0:
+                if max_e < linea[2]:
+                    max_e = linea[2]
+            else:
+                max_e = linea[2]
+
+    letters_list = ["A", "B", "C", "D", "E"]
+    max_list = [int(max_a), int(max_b), int(max_c), int(max_d), int(max_e)]
+    min_list = [int(min_a), int(min_b), int(min_c), int(min_d), int(min_e)]
+
+    x = list(zip(letters_list,max_list, min_list))
+    return x
 
 
 def pregunta_06():
@@ -221,7 +310,182 @@ def pregunta_06():
     ]
 
     """
-    return
+    import pandas as pd
+    import numpy as np
+    def load_data():
+        """Retorna los registros con los campos de interes como una lista de tuplas."""
+
+        #csvfile = open("/content/drive/MyDrive/Capacitaciones/IA/Postgrado/ciencia de datos/Lab1/programacion-en-python-mblandonp/data.csv", "r")
+        x = open("data.csv", "r").readlines()
+        x = [z.split("\t") for z in x]
+        x = [z[4] for z in x[:]]
+        x = [z.replace("\n", "") for z in x]
+        x = [z.split(",") for z in x[:]]
+
+        return x
+
+    
+    data = load_data()
+    df = pd.DataFrame(data)
+    data = df.to_numpy().flatten()
+    res = pd.DataFrame(data[data != None])
+    res = columns =['Name']
+    res = str.split(':', expand = True,n=1)
+    resdiv = res.columns =['Name','Value']
+    products_list = res.values.tolist()
+    TypesNames = sorted({row[0] for row in products_list[::]})
+
+    min_a = 0
+    min_b = 0
+    min_c = 0
+    min_d = 0
+    min_e = 0
+    min_f = 0
+    min_g = 0
+    min_h = 0
+    min_i = 0
+    min_j = 0
+
+    max_a = 0
+    max_b = 0
+    max_c = 0
+    max_d = 0
+    max_e = 0
+    max_f = 0
+    max_g = 0
+    max_h = 0
+    max_i = 0
+    max_j = 0
+
+    for linea in products_list:
+        if linea[0] == "aaa":
+            if min_a != 0:
+                if min_a > linea[1]:
+                    min_a = linea[1]
+            else:
+                min_a = linea[1]
+        elif linea[0] == "bbb":
+            if min_b != 0:
+                if min_b > linea[1]:
+                    min_b = linea[1]
+            else:
+                min_b = linea[1]
+        elif linea[0] == "ccc":
+            if min_c != 0:
+                if min_c > linea[1]:
+                    min_c = linea[1]
+            else:
+                min_c = linea[1]
+        elif linea[0] == "ddd":
+            if min_d != 0:
+                if min_d > linea[1]:
+                    min_d = linea[1]
+            else:
+                min_d = linea[1]
+        elif linea[0] == "eee":
+            if min_e != 0:
+                if min_e > linea[1]:
+                    min_e = linea[1]
+            else:
+                min_e = linea[1]
+        elif linea[0] == "fff":
+            if min_f != 0:
+                if min_f > linea[1]:
+                    min_f = linea[1]
+            else:
+                min_f = linea[1]
+        elif linea[0] == "ggg":
+            if min_g != 0:
+                if min_g > linea[1]:
+                    min_g = linea[1]
+            else:
+                min_g = linea[1]
+        elif linea[0] == "hhh":
+            if min_h != 0:
+                if min_h > linea[1]:
+                    min_h = linea[1]
+            else:
+                min_h = linea[1]
+        elif linea[0] == "iii":
+            if min_i != 0:
+                if min_i > linea[1]:
+                    min_i = linea[1]
+            else:
+                min_i = linea[1]
+        elif linea[0] == "jjj":
+            if min_j != 0:
+                if min_j > linea[1]:
+                    min_j = linea[1]
+            else:
+                min_j = linea[1]
+
+    for linea in products_list:
+        if linea[0] == "aaa":
+            if max_a != 0:
+                if max_a < linea[1]:
+                    max_a = linea[1]
+            else:
+                max_a = linea[1]
+        elif linea[0] == "bbb":
+            if max_b != 0:
+                if max_b < linea[1]:
+                    max_b = linea[1]
+            else:
+                max_b = linea[1]
+        elif linea[0] == "ccc":
+            if max_c != 0:
+                if max_c < linea[1]:
+                    max_c = linea[1]
+            else:
+                max_c = linea[1]
+        elif linea[0] == "ddd":
+            if max_d != 0:
+                if max_d < linea[1]:
+                    max_d = linea[1]
+            else:
+                max_d = linea[1]
+        elif linea[0] == "eee":
+            if max_e != 0:
+                if max_e < linea[1]:
+                    max_e = linea[1]
+            else:
+                max_e = linea[1]
+        elif linea[0] == "fff":
+            if max_f != 0:
+                if max_f < linea[1]:
+                    max_f = linea[1]
+            else:
+                max_f = linea[1]
+        elif linea[0] == "ggg":
+            if max_g != 0:
+                if max_g < linea[1]:
+                    max_g = linea[1]
+            else:
+                max_g = linea[1]
+        elif linea[0] == "hhh":
+            if max_h != 0:
+                if max_h < linea[1]:
+                    max_h = linea[1]
+            else:
+                max_h = linea[1]
+        elif linea[0] == "iii":
+            if max_i != 0:
+                if max_i < linea[1]:
+                    max_i = linea[1]
+            else:
+                max_i = linea[1]
+        elif linea[0] == "jjj":
+            if max_j != 0:
+                if max_j < linea[1]:
+                    max_j = linea[1]
+            else:
+                max_j = linea[1]
+    
+    min_lista = [int(min_a), int(min_b), int(min_c), int(min_d), int(min_e), int(min_f), int(min_g), int(min_h), int(min_i), int(min_j)]
+    max_lista = [int(max_a), int(max_b), int(max_c), int(max_d), int(max_e), int(max_f), int(max_g), int(max_h), int(max_i), int(max_j)]
+    yy = list(zip(TypesNames,min_lista,max_lista))
+
+    return yy
 
 
 def pregunta_07():
